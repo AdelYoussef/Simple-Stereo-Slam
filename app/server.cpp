c52f835968eb094d8e4043318eccef89764f8232
Adel Youssef <63298005+AdelYoussef@users.noreply.github.com>
2024-03-20 15:37:09 +0200

    Add files via upload

--- app/server.cpp
+++ app/server.cpp
@@ -2,17 +2,23 @@
 
 #include"../include/frame.h"
 #include"../include/feature.h"
-//#include"../include/map.h"
+#include"../include/map.h"
 #include"../include/mappoint.h"
+#include"../include/frontend.h"
+#include"../include/pinhole_camera.h"
+#include"../include/algorithms.h"
 
 int main() {
 
 
-    std::cout<<"building GL SLAM  YA GAAAAAAMED"<<std::endl;
+    std::cout<<"building GL SLAM  YA GAAAAAAMED BGD WALLAHY"<<std::endl;
     GL_SLAM::frame_test();
     GL_SLAM::feature_test();
-    //GL_SLAM::map_test();
+    GL_SLAM::map_test();
     GL_SLAM::mappoint_test();
+    GL_SLAM::frontend_test();
+    GL_SLAM::test_pinhole();
+    GL_SLAM::test_algorithms();
     
     return 0;
 }
